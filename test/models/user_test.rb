87f6080f101cc762449c89c4d84332d975188981
yiyiarrow <yiyiarrow@163.com>
2015-05-19 15:38:10 +0000

    Regex and validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,4 +16,35 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "  "
     assert_not @user.valid?
   end  
+
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end  
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end  
+
+  test "email should not be too long" do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end  
+
+  test "email validation shoud accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation shoud reject valid addresses" do
+    valid_addresses = %w[user@example,com user_foo_org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert_not @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
 end

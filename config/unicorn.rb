f43213492389c0ff71efb317d9241b72f2938864
yiyiarrow <yiyiarrow@163.com>
2015-05-31 10:57:04 +0000

    modify config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-  timeout 15
-  preload_app true
+timeout 15
+preload_app true
 
 before_fork do |server, worker| 
   Signal.trap 'TERM' do
@@ -10,7 +10,7 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect! 
 end
-￼￼
+
 after_fork do |server, worker| 
   Signal.trap 'TERM' do
     msg  = 'Unicorn worker intercepting TERM and doing nothing. '

1ff355285fe101e16e46eeceb376ecd7075f5f9a
yiyiarrow <yiyiarrow@163.com>
2015-05-31 05:16:49 +0000

    Make a basic User Model(including secure password)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -47,4 +47,16 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
+
+  test "email addresses should be unique" do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save
+      assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end  
 end
